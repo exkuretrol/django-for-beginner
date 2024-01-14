78134b606f5bac3ddf4e45782b1862bb385d80fa
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-01-14 13:57:32 +0800

    feat: add some basic template like home, about and some test

--- Chapter 3/pages/views.py
+++ Chapter 3/pages/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
-def home_page_view(request):
-    return HttpResponse("Hi")
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
