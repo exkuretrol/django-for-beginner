2994a2b949822e40649f46bade0c2772b051e7a7
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-01-15 16:56:41 +0800

    feat: finish chapter 7

--- ch-7-forms/blog/tests.py
+++ ch-7-forms/blog/tests.py
@@ -12,9 +12,7 @@ class BlogTest(TestCase):
         )
 
         cls.post = Post.objects.create(
-            title="test post title",
-            author=cls.user,
-            body="post body"
+            title="test post title", author=cls.user, body="post body"
         )
 
     def test_post_model(self):
@@ -31,7 +29,7 @@ class BlogTest(TestCase):
     def test_url_exists_at_correct_location_detailview(self):
         response = self.client.get("/post/1/")
         self.assertEqual(response.status_code, 200)
-    
+
     def test_post_listview(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
@@ -46,3 +44,33 @@ class BlogTest(TestCase):
         self.assertContains(response, "post body")
         self.assertTemplateUsed(response, "post_detail.html")
 
+    def test_post_createview(self):
+        response = self.client.post(
+            reverse("post_new"),
+            {
+                "title": "test post title",
+                "author": self.user.id,
+                "body": "test post body",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "test post title")
+        self.assertEqual(Post.objects.last().body, "test post body")
+
+    def test_post_updateview(self):
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {
+                "title": "test post new title",
+                "body": "test post new body",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "test post new title")
+        self.assertEqual(Post.objects.last().body, "test post new body")
+
+    def test_post_deleteview(self):
+        response = self.client.post(
+            reverse("post_delete", args="1"),
+        )
+        self.assertEqual(response.status_code, 302)
